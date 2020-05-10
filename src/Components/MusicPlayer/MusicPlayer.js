24ef8c9d4558a260a844db7fa66f3069cdc7f0e2
John Marsden <john.marsden24@gmail.com>
2020-05-10 13:12:04 +0100

    added music player

--- src/Components/MusicPlayer/MusicPlayer.js
+++ src/Components/MusicPlayer/MusicPlayer.js
@@ -1,21 +1,6 @@
 import React from 'react';
 
-const links = {
-  'Calm With Horses': "4YmWmoshc1KRw6Q4zM2idj",
-  'Animated Violence Mild': "74tQiCbJ97DTI7zsRbj55f",
-  'World Eater': "6WElx5zuqd2CYiyea4oEc8"
-}
-
 class MusicPlayer extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { src:  };
-  //   this.setAlbum = this.setAlbum.bind(this);
-  // }
-
-  setAlbum(albumLink) {
-    this.setState({src: albumLink})
-  }
 
   render() {
     return (
@@ -27,5 +12,3 @@ class MusicPlayer extends React.Component {
 }
 
 export default MusicPlayer
-
-
